b9e7deb0974a22a4d24ad06e55d44332aeb5bdb3
snehapatra454 <168355859+snehapatra454@users.noreply.github.com>
2024-06-30 20:08:48 +0530

    Add files via upload

---
+++ context.jsx
@@ -0,0 +1,27 @@
+import { createContext } from "react";
+import runChat from "../config/gemini";
+
+
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+    
+    const onSent =async (prompt) => {
+      await runChat(prompt)
+    }
+    
+    onSent("What is react JS")
+
+    const contextValue = {
+        
+    }
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProvider;
